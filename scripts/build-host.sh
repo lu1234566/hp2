c82bf98e12025b733f0dc1a2699119073c828fb9
lu1234566 <barcelar34@gmail.com>
2026-08-12 12:21:40 -0300

    Build skeletal probe in private host toolchain

--- scripts/build-host.sh
+++ scripts/build-host.sh
@@ -12,6 +12,7 @@ common_sources=(
     "$ROOT/src/portcore/ue_mesh.cpp"
     "$ROOT/src/portcore/ue_model.cpp"
     "$ROOT/src/portcore/ue_package.cpp"
+    "$ROOT/src/portcore/ue_skeletal.cpp"
     "$ROOT/src/portcore/ue_texture.cpp"
 )
 common_flags=(
@@ -30,7 +31,14 @@ g++ "${common_flags[@]}" "${common_sources[@]}" \
     "$ROOT/tools/hp2_map_probe.cpp" -o "$BUILD_DIR/hp2_map_probe"
 
 g++ "${common_flags[@]}" "${common_sources[@]}" \
+    "$ROOT/tools/hp2_skeletal_probe.cpp" -o "$BUILD_DIR/hp2_skeletal_probe"
+
+g++ "${common_flags[@]}" "${common_sources[@]}" \
     "$ROOT/tests/portcore_tests.cpp" -o "$BUILD_DIR/portcore_tests"
 
+g++ "${common_flags[@]}" "${common_sources[@]}" \
+    "$ROOT/tests/skeletal_probe_tests.cpp" -o "$BUILD_DIR/skeletal_probe_tests"
+
 "$BUILD_DIR/portcore_tests"
+"$BUILD_DIR/skeletal_probe_tests"
 echo "Host tools: $BUILD_DIR"
